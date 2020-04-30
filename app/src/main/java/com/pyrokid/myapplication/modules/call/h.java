0b1ee6bb29b7fff5c2d460db9651294c5e5ef913
Melkov N.D <melkov.n.d@gmail.com>
2020-04-30 15:38:32 +0700

    sdk and sinch service implemented

---
+++ app/src/main/java/com/pyrokid/myapplication/modules/call/h.java
@@ -0,0 +1,4 @@
+package com.pyrokid.myapplication.modules.call;
+
+public class h {
+}
---
+++ app/src/main/java/com/pyrokid/myapplication/modules/call/incomingCall/IncomingCallActivity.java
@@ -0,0 +1,10 @@
+package com.pyrokid.myapplication.modules.call.incomingCall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IncomingCallActivity extends AppCompatActivity {
+
+
+
+
+}
